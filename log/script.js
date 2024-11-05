fa1a78f601d240c3b2838e805ba7dc7c0039ea91
Shiina <quangtungnguyen613@gmail.com>
2024-11-05 18:33:15 +0700

    add code lan 2

--- log/script.js
+++ log/script.js
@@ -1,22 +1,17 @@
-function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
-
-    // Ẩn tất cả các tab content
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-    // Xóa class "active" ở tất cả các nút
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Hiển thị tab được chọn và thêm class "active" vào nút được chọn
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+function showRegister() {
+    document.getElementById("login-box").style.display = "none";
+    document.getElementById("register-box").style.display = "block";
+    document.getElementById("forgot-password-box").style.display = "none";
 }
 
-// Mở tab "Đăng Nhập" mặc định
-document.getElementById("defaultOpen").click();
+function showLogin() {
+    document.getElementById("register-box").style.display = "none";
+    document.getElementById("login-box").style.display = "block";
+    document.getElementById("forgot-password-box").style.display = "none";
+}
+
+function showForgotPassword() {
+    document.getElementById("login-box").style.display = "none";
+    document.getElementById("register-box").style.display = "none";
+    document.getElementById("forgot-password-box").style.display = "block";
+}
